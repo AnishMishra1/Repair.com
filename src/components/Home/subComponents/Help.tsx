58b1e0b9a1c014284ef77899ac4285b07d765f60
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-21 20:01:24 +0530

    changing font style

--- src/components/Home/subComponents/Help.tsx
+++ src/components/Home/subComponents/Help.tsx
@@ -1,60 +1,54 @@
 // Dependencies
-import { Image, Button } from "@nextui-org/react";
-import { FaArrowCircleRight } from "react-icons/fa";
-import { IoIosInformationCircle } from "react-icons/io";
-import { Divider } from "@nextui-org/react";
-
-
 
 const Help = () => {
   return (
     <div className="flex flex-row items-center justify-between bg-gray-200 h-auto ">
-      <div className="md:px-[5rem] px-[1rem] py-[2rem]">
+      <div className="md:px-[5rem] px-[2rem] py-[2rem]">
         <div className="py-[1rem]">
           <h1 className="text-blue-500 text-3xl font-sans font-semibold">
             Plumbing Repair Services in Upland, CA & the Surrounding Areas
           </h1>
         </div>
         <div className="py-[0.5rem] md:px-[0rem] ">
-          <p className="text-black font-serif">
+          <p className="text-black font-['Roboto'] text-justify">
             Are your faucets starting to leak? Do you notice problems as your toilet flushes or with your sink drains?
             It might be time to reach out to your local plumbing repair company. For Upland, CA, residents, the AYS
             Plumbing & Rooter team is here to help fix all your plumbing problems.
           </p>
-          <p className="text-black font-serif py-[0.5rem]">
+          <p className="text-black font-['Roboto'] text-justify py-[0.5rem]">
             Have you thought about what a good plumber can do for you? Our team maximizes all the benefits of a good
             plumber without any downsides. Instead of waiting days for your repairs, we’re ready and on call at a
             moment’s notice. Everything else takes a backseat when your appliances need fixing, and we’ll show you what
             genuine customer care and attention to detail look like.
           </p>
-          <p className="text-black font-serif py-[0.5rem]">
+          <p className="text-black font-['Roboto'] text-justify py-[0.5rem]">
             Contact us today for plumbing repair services in Upland, CA, and the surrounding areas, and get back to
             enjoying the convenience of all the plumbing appliances in your home. Check out our service areas below!
             We’re pleased to serve residents all over CA, including the following locations:
           </p>
-          <div className="flex flex-row justify-start items-center gap-4 text-black font-serif py-[0.5rem]">
+          <div className="flex flex-row justify-start items-center gap-4 text-black font-['Roboto'] py-[0.5rem]">
             <div className="px-[1rem]">
               <ul className="list-disc">
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
+                <li>Ontario, CA</li>
+                <li>Montclair, CA</li>
+                <li>Rancho Cucamonga, CA</li>
+                <li>Claremont, CA</li>
+                <li>Chino, CA</li>
+                <li>Pomona, CA</li>
+                <li>La Verne, CA</li>
+                <li>Chino Hills, CA</li>
               </ul>
             </div>
-            <div className="px-[5rem]">
+            <div className="md:px-[5rem]">
               <ul className="list-disc">
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
-                <li>San Bernardino</li>
+                <li>San Dimas, CA</li>
+                <li>Diamond Bar, CA</li>
+                <li>Mira Loma, CA</li>
+                <li>Glen Avon, CA</li>
+                <li>Charter Oak, CA</li>
+                <li>Fontana, CA</li>
+                <li>Glendora, CA</li>
+                <li>San Bernardino, CA</li>
               </ul>
             </div>
           </div>
