126c499c10e1ce70093b7557e0b513f4c25a693d
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-26 17:30:38 +0530

    adding robinhood

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,9 @@ import { useSelector } from "react-redux";
 // Local Files
 import NavBar from "./globalSubComponents/NavBar";
 import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import FAQ from "./components/FAQ/FAQ";
-import Auth from "./components/Auth/Auth";
-import Investment from "./components/Investment/Investment";
-import Crypto from "./components/Crypto/Crypto";
-import Options from "./components/Options/Options";
-import Retirement from "./components/Retirement/Retirement";
 import Footer from "./globalSubComponents/Footer";
 import { RootState } from "./store/store";
-import Privacy from "./components/Privacy/Privacy";
+import Robinhood from "./components/Robinhood/Robinhood";
 
 
 function App() {
@@ -23,22 +15,14 @@ function App() {
 
   return (
     <>
-      {curTab === "Auth" ? null : <NavBar />}
+      {curTab === "Auth" || curTab === "Robinhood" ? null : <NavBar />}
       <Routes>
         <Route path="/" element={<Navigate to="/Home" />} />
         <Route path="/Home" element={<Home />} />
-        {/* <Route path="/About" element={<About />} />
-        <Route path="/Privacy" element={<Privacy />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/FAQ" element={<FAQ />} />
-        <Route path="/Auth" element={<Auth />} />
-        <Route path="/Investment" element={<Investment />} />
-        <Route path="/Crypto" element={<Crypto />} />
-        <Route path="/Options" element={<Options />} />
-        <Route path="/Retirement" element={<Retirement />} /> */}
+        <Route path="/Robinhood" element={<Robinhood />} />
         <Route path="*" element={<Navigate to="/Home" />} />
       </Routes>
-      {curTab === "Auth" ? null : <Footer />}
+      {curTab === "Auth" || curTab === "Robinhood" ? null : <Footer />}
     </>
   );
 }
--- src/components/Robinhood/Robinhood.tsx
+++ src/components/Robinhood/Robinhood.tsx
@@ -1,20 +1,21 @@
 // Dependencies
 import { useDispatch } from "react-redux";
-
-// Local Files
 import { updateTab } from "../../store/curTabSlice";
+
+import RobinNav from "./subComponents/RobinNav";
+import Footer from "./subComponents/Footer";
 import HeroSection from "./subComponents/HeroSection";
-import Achievement from "./subComponents/Achievement";
-import Help from "./subComponents/Help";
-import Youtube from "./subComponents/Youtube";
 import Testimonials from "./subComponents/Testimonials";
+import Help from "./subComponents/Help";
 import Protection from "./subComponents/Protection";
 import Join from "./subComponents/Join";
-import Card from "./subComponents/Card";
+import Achievement from "./subComponents/Achievement";
+import Youtube from "./subComponents/Youtube";
+import Popover from "./subComponents/Popover";
 
-const Home = () => {
+const Robinhood = () => {
   const dispatch = useDispatch();
-  dispatch(updateTab("Home"));
+  dispatch(updateTab("Robinhood"));
 
   window.scrollTo({
     top: 0,
@@ -23,16 +24,18 @@ const Home = () => {
 
   return (
     <div>
+      <Popover />
+      <RobinNav />
       <HeroSection />
-      <Card/>
-      <Help />
-      <Join />
-      <Protection />
-      <Youtube />
       <Testimonials />
+      <Help />
+      <Protection />
+      <Join />
       <Achievement />
+      <Youtube />
+      <Footer />
     </div>
   );
 };
 
-export default Home;
+export default Robinhood;
